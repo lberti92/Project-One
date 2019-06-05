1a40c9db5106b1be6d2a5655f159eb2548b72104
Breeze <lberti@msn.com>
2019-06-04 20:20:25 -0600

    adding Endamam api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,7 +19,7 @@ $.ajax({
         var lon = response.results[0].geometry.location.lng;
 
         // ZOMATO
-        var key = "5f7e180716b2eb861d47aa1baa40e686"
+        var zomatoKey = "5f7e180716b2eb861d47aa1baa40e686"
 
         // // NOTE: you can only run the code below once you have the geocoded lat/lon from Google Geocoder...
         // // for that reason, you'll likely run this code in the .then for the geocoder API call:
@@ -34,7 +34,7 @@ $.ajax({
             url: cuisinesQuery,
             // the Zomato API is expecting the 'user-key' to be passed in as a header
             beforeSend: function (request) {
-                request.setRequestHeader("user-key", key);
+                request.setRequestHeader("user-key", zomatoKey);
             },
             // after the cuisine data comes back from the API...
         }).then(function (response) {
@@ -55,56 +55,60 @@ $.ajax({
                 console.log("selected " + selected);
                 // user the human speak cuisine to find the special Zomato cuisine number
                 // (hint: you'll need this for the final Zomato query!)
-                alert($("#cuisines [value='" + selected + "']").data('cuisine-id'));
+                // alert($("#cuisines [value='" + selected + "']").data('cuisine-id'));
                 // at this point in the game, you would now have both the lat/lon AND the cuisine id,
                 // which should be everything needed for the AJAX calls to either 1) search for 
                 // restaurants or 2) search for recipes 
                 console.log("restaurant lat " + lat);
                 console.log("restaurant lng " + lon);
                 console.log("restaurant cruisine " + selected);
-               
-           
-
-            // All have been resolved (or rejected), do your thing
 
 
-            // After submit button has been clicked and we have received the "selected id" then 
-            //Restaurants (Zomato) or Recipes (Endamam) will need to be displayed
-            var restaurantQuery = "https://developers.zomato.com/api/v2.1/search?count=5&lat=" + lat + "&lon=" + lon + "&cuisines=" + selected;
-            $.ajax({
-                url: restaurantQuery,
-                method: "GET",
-                async: false,
-                beforeSend: function (request) {
-                    request.setRequestHeader("user-key", key);
-                },
-            }).then(function (response) {
-                console.log(response);
-                console.log(response.restaurants[0].restaurant.name);
-                console.log(response.restaurants[1].restaurant.name);
-                console.log(response.restaurants[2].restaurant.name);
-                console.log(response.restaurants[3].restaurant.name);
-                console.log(response.restaurants[4].restaurant.name);
+
+                // All have been resolved (or rejected), do your thing
+
+
+                // After submit button has been clicked and we have received the "selected id" then 
+                //Restaurants (Zomato) or Recipes (Endamam) will need to be displayed
+                var restaurantQuery = "https://developers.zomato.com/api/v2.1/search?count=5&lat=" + lat + "&lon=" + lon + "&cuisines=" + selected;
+                $.ajax({
+                    url: restaurantQuery,
+                    method: "GET",
+                    async: false,
+                    beforeSend: function (request) {
+                        request.setRequestHeader("user-key", zomatoKey);
+                    },
+                }).then(function (response) {
+                    console.log(response);
+
+                    //create a var array
+                    console.log(response.restaurants[0].restaurant.name);
+                    console.log(response.restaurants[1].restaurant.name);
+                    console.log(response.restaurants[2].restaurant.name);
+                    console.log(response.restaurants[3].restaurant.name);
+                    console.log(response.restaurants[4].restaurant.name);
+                });
+
+                var edamamQueryURL = "https://api.edamam.com/search?q=" + selected + "&app_id=e4946987&app_key=ee70be41f697b3bd702e4e02fc258d39";
+
+
+                $.ajax({
+                    url: edamamQueryURL,
+                    method: "GET"
+                }).then(function (response) {
+                    console.log(response);
+                });
+
+                // get summary view of 5 results rendering
+
+                // create button with jQuery to show detail of each result
+
+                // show detail on button submit in container of detail view
             });
+
         });
+
     });
 
-});
-// set up API for Edamam
-var edamamSearchResults = $(this).attr("data-name");
-
-var edamamQueryURL = "https://api.edamam.com/search?q=" + edamamSearchResults + "&app_id=e4946987&app_key=ee70be41f697b3bd702e4e02fc258d39";
 
 
-$.ajax({
-    url: edamamQueryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-});
-
-// get summary view of 5 results rendering
-
-// create button with jQuery to show detail of each result
-
-// show detail on button submit in container of detail view
