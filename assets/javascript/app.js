29fb76a6cf377d4c8845c4f8c52bbcafdcf6fb0d
DiabloAzul <curr33@gmail.com>
2019-06-08 12:18:18 -0600

    merging updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,7 +15,7 @@ $("#submitLoc").on("click", function (event) {
     event.preventDefault();
     var address = $("#cuisine-location").val();
     console.log(address);
-    var geoURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=" + geoAPI4;
+    var geoURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=" + geoAPI;
     $(".cuisines").toggle();
 
     $.ajax({
